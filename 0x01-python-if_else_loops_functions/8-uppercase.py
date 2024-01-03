8fea0a1e39ce08b4a3fc0bfafa070945aeee3b35
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-03 03:48:31 +0200

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            upper = chr(ord(char) - 32)
+            new += upper
+        else:
+            new += char
+    print(new)
