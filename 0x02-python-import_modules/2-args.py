67a450238632d019fe29da0345798497786a40e3
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-05 01:29:35 +0200

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    args = len(argv) - 1
+    print("{:d} argument{}{}".format(args, 's' if args != 1 else '',
+                                     '.' if args == 0 else ':'))
+    
+    for i in range(1, args + 1):
+        print("{:d}: {}".format(i, argv[i]))
