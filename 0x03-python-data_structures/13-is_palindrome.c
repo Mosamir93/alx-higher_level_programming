2e4b9913cffea8967c470e1fbfd2df03dd44e3d7
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-08 15:08:07 +0200

    13.2

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -27,7 +27,7 @@ int is_palindrome(listint_t **head)
 	if (k % 2 != 0)
 		k += 1;
 
-	for (i = 0; i <= (len / 2); i++)
+	for (i = 0; i <= k; i++)
 	{
 		sm_half = sm_half->next;
 		s_half = s_half->next;
