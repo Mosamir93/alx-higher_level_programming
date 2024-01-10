22191a30d86b94b67cf27a257293d1c783075a9f
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-11 00:15:38 +0200

    103.9

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,55 +1,59 @@
+#include <stdio.h>
 #include <Python.h>
 
-/**
- * print_python_list - print some basic info about Python lists
- * @p: pointer to the python list
- */
-
-void print_python_bytes(PyObject *p);
-
-void print_python_list(PyObject *p)
-{
-	int i;
-	PyListObject *pp;
-
-	pp = (PyListObject *)p;
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", ((PyVarObject *)p)->ob_size);
-	printf("[*] Allocated = %ld\n", pp->allocated);
-	for (i = 0; i < pp->ob_base.ob_size; i++)
-	{
-		printf("Element %d: %s\n", i, pp->ob_item[i]->ob_type->tp_name);
-		if (strcmp(pp->ob_item[i]->ob_type->tp_name, "bytes") == 0)
-			print_python_bytes(pp->ob_item[i]);
-	}
-}
 
 /**
  * print_python_bytes - print some basic info about Python bytes objects
- * @p: pointer to the python list
- */
+ * @p: python object
+ * Return: nothing
+ **/
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i;
-	char *str;
+	char *s;
+	Py_ssize_t len, i;
 
 	printf("[.] bytes object info\n");
-	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
-	{
+	if (!PyBytes_Check(p))
 		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
-	size = ((PyVarObject *)p)->ob_size;
-	str = ((PyBytesObject *)p)->ob_sval;
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", str);
-	printf("  first %ld bytes: ", size + 1 <= 10 ? size + 1 : 10);
-	for (i = 0; i <= size && i < 10; i++)
+	else
 	{
-		printf("%02hhx", str[i]);
-		if (i <= size - 1)
-			printf(" ");
+		PyBytes_AsStringAndSize(p, &s, &len);
+		printf("  size: %lu\n", len);
+		printf("  trying string: %s\n", s);
+		if (len > 10)
+			len = 10;
+		else
+			len++;
+		printf("  first %lu bytes: ", len);
+		for (i = 0; i < len - 1; i++)
+			printf("%02x ", s[i] & 0xff);
+		printf("%02x\n", s[len - 1] & 0xff);
 	}
-	printf("\n");
+}
+
+
+/**
+ * print_python_list - print some basic info about Python lists
+ * @p: python object
+ * Return: nothing
+ **/
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t i;
+	PyObject *in_list;
+
+	if (PyList_Check(p))
+	{
+		printf("[*] Python list info\n");
+		printf("[*] Size of the Python List = %lu\n", PyList_Size(p));
+		printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+		for (i = 0; i < PyList_Size(p); i++)
+		{
+			in_list = PySequence_GetItem(p, i);
+			printf("Element %lu: %s\n", i,
+					in_list->ob_type->tp_name);
+			if (strcmp(in_list->ob_type->tp_name, "bytes") == 0)
+				print_python_bytes(in_list);
+		}
+	}
 }
---
+++ 0x04-python-more_data_structures/2103-python.c
@@ -0,0 +1,55 @@
+#include <Python.h>
+
+/**
+ * print_python_list - print some basic info about Python lists
+ * @p: pointer to the python list
+ */
+
+void print_python_bytes(PyObject *p);
+
+void print_python_list(PyObject *p)
+{
+	int i;
+	PyListObject *pp;
+
+	pp = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", ((PyVarObject *)p)->ob_size);
+	printf("[*] Allocated = %ld\n", pp->allocated);
+	for (i = 0; i < pp->ob_base.ob_size; i++)
+	{
+		printf("Element %d: %s\n", i, pp->ob_item[i]->ob_type->tp_name);
+		if (strcmp(pp->ob_item[i]->ob_type->tp_name, "bytes") == 0)
+			print_python_bytes(pp->ob_item[i]);
+	}
+}
+
+/**
+ * print_python_bytes - print some basic info about Python bytes objects
+ * @p: pointer to the python list
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size, i;
+	char *str;
+
+	printf("[.] bytes object info\n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	size = ((PyVarObject *)p)->ob_size;
+	str = ((PyBytesObject *)p)->ob_sval;
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", str);
+	printf("  first %ld bytes: ", size + 1 <= 10 ? size + 1 : 10);
+	for (i = 0; i <= size && i < 10; i++)
+	{
+		printf("%02hhx", str[i]);
+		if (i <= size - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
