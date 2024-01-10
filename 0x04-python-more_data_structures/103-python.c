9ef1a66c99d02dd04fd651fc8ea7e2ea65ec8a35
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-10 23:44:46 +0200

    103.4

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -16,7 +16,11 @@ void print_python_list(PyObject *p)
 	printf("[*] Size of the Python List = %ld\n", pp->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", pp->allocated);
 	for (i = 0; i < pp->ob_base.ob_size; i++)
+	{
 		printf("Element %d: %s\n", i, pp->ob_item[i]->ob_type->tp_name);
+		if (strcmp(pp->ob_item[i]->ob_type->tp_name, "bytes") == 0)
+			print_python_bytes(pp->ob_item[i]);
+	}
 }
 
 /**
@@ -25,7 +29,7 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i;
+	unsigned char size, i;
 	char *str;
 
 	printf("[.] bytes object info\n");
