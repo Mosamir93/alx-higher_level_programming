6ea577d6086a8a1acfb301355c7a390ec5dbf90e
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-23 22:14:50 +0200

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines Square class."""
+
+
+class Square:
+    """Class definition."""
+    def __init__(self, size):
+        """Initializes instance.
+
+        Args:
+            size: size of a square
+        """
+        self.__size = size
