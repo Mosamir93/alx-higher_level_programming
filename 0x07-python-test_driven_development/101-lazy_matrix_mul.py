7d1374f2438ce1100925dddef776403fcee88999
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-31 02:25:02 +0200

    0

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Matrix multiplication numpy module."""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function definition."""
+    return numpy.matmul(m_a, m_b)
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Text_indentation method."""
+
+
+def text_indentation(text):
+    """function that prints a text with 2 new lines after ., ? and :"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delim in ".?:":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
