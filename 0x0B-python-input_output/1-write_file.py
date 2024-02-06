ca1a4c2adff22d1db558c5f71abf27e931780207
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-02-07 01:58:47 +0200

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write module."""
+
+
+def write_file(filename="", text=""):
+    """Function definition."""
+    with open(filename, mode="w", encoding="utf-8") as file:
+       return file.write(text)
