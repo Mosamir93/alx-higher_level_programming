553dfbcab1cdc88c3681cb0c4832aab71ca1e1e6
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-02-07 02:54:30 +0200

    7.5

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """Adds arguments to a python list."""
-import sys
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 file = "add_item.json"
 list = []
+
 try:
     list = load_from_json_file(file)
 except Exception:
     save_to_json_file(list, file)
+
 list.extend(sys.argv[1:])
 save_to_json_file(list, file)
