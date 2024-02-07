59b55b393d6fc9dfd1cff433f8b8b60eb232ff7a
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-02-07 04:41:51 +0200

    101.4

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,25 +4,45 @@
 
 def print_stats(total_size, status_counts):
     """Function definition."""
-    sorted_keys = sorted(status_codes.keys())
-    print('\n'.join(["{:d}: {:d}".format(k, status_codes[k])
-                     for k in sorted_keys if status_codes[k] != 0]))
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
     import sys
 
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
     try:
-        total = 0
-        status_codes = \
-            {code: 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
-        for n, line in enumerate(sys.stdin, 1):
-            words = line.split()
-            total += int(words[-1])
-            status_codes[int(words[-2])] += 1
-            if n % 10 == 0:
-                print("File size: {:d}".format(total))
-                print_dict_sorted_nonzero(status_codes)
-    finally:
-        print("File size: {:d}".format(total))
-        print_dict_sorted_nonzero(status_codes)
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
