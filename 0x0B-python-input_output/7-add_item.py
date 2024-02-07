a2096249026184cd93d321c69b0fa3fb615a5195
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-02-07 02:50:33 +0200

    7.3

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Adds arguments to a python list."""
 import sys
-save_to_json_file = __import__(5-save_to_json_file.py).save_to_json_file
-load_from_json_file = __import__(6-load_from_json_file.py).load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
 
 file = "add_item.json"
