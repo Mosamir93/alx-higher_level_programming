3672b02aeda8e5909dba8f99a56711c0f0ed3943
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-02-07 02:02:02 +0200

    2.1

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Append module."""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Function definition."""
     with open(filename, mode="a", encoding="utf-8") as file:
         return file.write(text)
