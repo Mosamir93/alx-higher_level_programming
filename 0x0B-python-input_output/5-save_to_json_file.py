88e30092cf196e0ba84bd5945c3070f1c0c2dfd4
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-02-07 02:24:10 +0200

    5.3

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,7 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Function definition."""
     with open(filename, mode="w") as file:
-        try:
-            json.dump(my_obj, file)
-        except TypeError:
-            pass
+        json.dump(my_obj, file)
