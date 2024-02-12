9814dc0ac168b73d65405749a7c208d84f26eede
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-02-13 00:25:35 +0200

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,21 +6,26 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Class definition."""
     def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor."""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Sets the __str__ of square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
+        """Size getter."""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Size setter."""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """Updates the attributes of an object."""
         if args and len(args) > 0:
             attrs = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
@@ -30,6 +35,7 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """Dictionary representation of an object."""
         return {
                 'id': self.id,
                 'x': self.x,
