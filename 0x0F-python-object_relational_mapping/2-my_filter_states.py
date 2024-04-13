ba838d153a31e49c492cb3c4302a8836bf1a03bc
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-04-13 07:36:40 +0200

    task-2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Get all states starting with N module."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """Script to get all states starting with N."""
+    usr = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=usr,
+                         passwd=passwd,
+                         db=db_name)
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states
+                   WHERE name = %s
+                   ORDER BY id""",
+                   (state_name,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
