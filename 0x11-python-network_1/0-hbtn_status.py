1c3618f8b9cd5c33dbd26edc040ff740bd61bb4a
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-04-26 15:58:41 +0300

    task-0.3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,12 @@
 """Fetches https://alx-intranet.hbtn.io/status."""
 from urllib import request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with request.urlopen(url) as res:
-    body = res.read()
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with request.urlopen(url) as res:
+        body = res.read()
 
-print("Body response:")
-print("    - type:", type(body))
-print("    - content:", body)
-print("    - utf8 content:", body.decode('utf-8'))
+    print("Body response:")
+    print("    - type:", type(body))
+    print("    - content:", body)
+    print("    - utf8 content:", body.decode('utf-8'))
