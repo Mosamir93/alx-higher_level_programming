9d83ec2f298a31d44a127433fc7faa43e9159997
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-21 15:50:37 +0300

    task-4.2

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,22 +3,19 @@
 const request = require('request');
 const character = 'https://swapi-api.alx-tools.com/api/people/18/';
 const url = process.argv[2];
-function countMovies (url, character) {
-  request.get(url, (err, response, body) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    if (response.statusCode === 200) {
-      const movies = JSON.parse(body).results;
-      let count = 0;
-      for (const movie of movies) {
-        if (movie.characters.includes(character)) {
-          count++;
-        }
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  if (response.statusCode === 200) {
+    const movies = JSON.parse(body).results;
+    let count = 0;
+    for (const movie of movies) {
+      if (movie.characters.includes(character)) {
+        count++;
       }
-      console.log(count);
     }
-  });
-}
-countMovies(url, character);
+    console.log(count);
+  }
+});
